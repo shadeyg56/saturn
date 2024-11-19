f6524161a9e91a83f184aefed80541fc17101138
shadeyg56 <shadeyg56@gmail.com>
2024-11-18 18:19:44 -0600

    Add nix flake
    Includes bundled package and dev shell

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A simple widget suite for Wayland built with Astal";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/18979df9f0be9d69f0e4d35059914c1a868c79b8";
+
+    ags = {
+      url = "github:aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs = { self, nixpkgs, ags }: 
+  let 
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    agsPkgs = ags.packages.${system};
+  in {
+
+    packages.${system}.default = ags.lib.bundle {
+      inherit pkgs;
+      src = ./.;
+      name = "saturn";
+      entry = "app.ts";
+
+      extraPackages = with agsPkgs; [
+        hyprland
+        wireplumber
+        battery
+        network
+        mpris
+        bluetooth
+        cava
+      ];
+    };
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [ agsPkgs.agsFull ];
+      shellHook = ''
+        # Generate types if they don't exist
+        if [ ! -d ./@girs ];
+          then ags types -d .
+        fi
+      '';
+    };
+  };
+}
